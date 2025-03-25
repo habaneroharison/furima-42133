24b984e53dfa80a57fc13fb525ca5667194966ad
habaneroharison <morimotoyuuki1102@yahoo.co.jp>
2025-03-25 19:22:04 +0900

    faker導入、テストコード改善

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
-    password_confirmation {'password'}
+    email                 {Faker::Internet.email}
+    password              {'test000'}
+    password_confirmation {'test000'}
     last_name             {'てすと'}
     first_name            {'てすと'}
     last_name_kana        {'テスト'}
