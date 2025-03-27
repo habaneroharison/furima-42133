67314484fcb73cf049d49d0cf22c427a5b81435d
habaneroharison <morimotoyuuki1102@yahoo.co.jp>
2025-03-27 17:04:24 +0900

    アクティブハッシュが選択された項目を保持

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,12 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new, status: :unprocessable_entity 
+    end
   end
-  
+
   private
 
 def item_params
-  params.require(:item).permit(:name, :description, :price, :category_id, :status_id, :shipping_fee_id, :delivery_time_id, :prefecture_id, :user_id)
+  params.require(:item).permit(:name, :description, :price, :category_id, :status_id, :shipping_fee_id, :delivery_time_id, :prefecture_id,:image).merge(user_id: current_user.id)
 end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,26 +1,35 @@
 class Item < ApplicationRecord
+
+  # ActiveHash関連付けを追加
+  include ActiveHash::Associations::ActiveRecordExtensions
+  
+
+
   # バリデーション設定
-  validates :name, presence: true  # 商品名は必須
-  validates :description, presence: true  # 商品説明は必須
-  validates :category_id, presence: true  # カテゴリーは必須
-  validates :status_id, presence: true  # 商品の状態は必須
-  validates :shipping_fee_id, presence: true  # 配送料の負担は必須
-  validates :prefecture_id, presence: true  # 発送元の地域は必須
-  validates :delivery_time_id, presence: true  # 発送までの日数は必須
-  validates :price, presence: true  # 価格は必須
-  validates :image, presence: true  # 画像は必須
+  validates :name, presence: { message: "can't be blank" }
+  validates :description, presence: { message: "can't be blank" }
+  validates :category_id, presence: { message: "can't be blank" }
+  validates :status_id, presence: { message: "can't be blank" }
+  validates :shipping_fee_id, presence: { message: "can't be blank" }
+  validates :prefecture_id, presence: { message: "can't be blank" }
+  validates :delivery_time_id, presence: { message: "can't be blank" }
+  validates :price, presence: { message: "can't be blank" }
+  validates :image, presence: { message: "can't be blank" }
   
   # 価格に関するバリデーション
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 300, 
+    message: "must be greater than or equal to 300" 
+  }
+  validates :price, numericality: { 
+    only_integer: true, 
+    less_than_or_equal_to: 9999999, 
+    message: "must be less than or equal to 9999999" 
+  }
   # 価格に関するバリデーション: 半角数値のみ許可
-  validates :price, numericality: { only_integer: true}
+  validates :price, numericality: { only_integer: true, message: "is not a number" }
   # ActiveStorageで画像を管理
   has_one_attached :image
-  
-  # 他の関連付けなど
-  belongs_to :category
-  belongs_to :status
-  belongs_to :shipping_fee
-  belongs_to :prefecture
-  belongs_to :delivery_time
+
 end
