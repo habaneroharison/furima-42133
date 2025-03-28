e6367642bb0f4cf2189b84abc0e34e034f556f9c
habaneroharison <morimotoyuuki1102@yahoo.co.jp>
2025-03-28 15:06:43 +0900

    itemテーブルからonly_integer: true,削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,12 +22,12 @@ class Item < ApplicationRecord
   
   # 価格に関するバリデーション
   validates :price, numericality: { 
-    only_integer: true, 
+  
     greater_than_or_equal_to: 300, 
     message: "must be greater than or equal to 300" 
   }
   validates :price, numericality: { 
-    only_integer: true, 
+  
     less_than_or_equal_to: 9999999, 
     message: "must be less than or equal to 9999999" 
   }
