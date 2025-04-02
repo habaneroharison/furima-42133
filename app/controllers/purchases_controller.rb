0c98922d407bfc7121771d664c4575345a099807
habaneroharison <morimotoyuuki1102@yahoo.co.jp>
2025-04-02 19:24:20 +0900

    鍵記述変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,6 +4,7 @@ class PurchasesController < ApplicationController
   before_action :redirect_if_seller, only: [:index, :create]
 
   def index
+
     if user_signed_in?
       @purchase_form = PurchaseForm.new
     else
@@ -18,11 +19,24 @@ class PurchasesController < ApplicationController
       @purchase_form.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
+
+  def purchase_params
+    params.require(:purchase_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,        # 商品の値段
+      card: purchase_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
+    )
+  end
 
   def set_item
     # パラメータからitem_idを取得し、対応するItemを取得
@@ -38,8 +52,4 @@ class PurchasesController < ApplicationController
     end
   end
 
-  def purchase_params
-    params.require(:purchase_form).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token)
-          .merge(user_id: current_user.id, item_id: params[:item_id])
-  end
 end
